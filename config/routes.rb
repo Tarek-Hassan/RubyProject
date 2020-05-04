05e88a0afd0ec6d2180aee220337beaa8921861f
amira <amira.a.a.hashim@gmail.com>
2020-05-04 11:43:25 +0200

    admin dashboard

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  authenticated :user do
+    # get 'home/index'
+    root to: 'home#index', as: :authenticated_root
+    # resources :users
+  end
+  root to: redirect('/users/sign_in')
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
