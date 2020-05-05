7ab39f378478400317e65ea48e4d15bfe8ff1f38
amira <amira.a.a.hashim@gmail.com>
2020-05-05 17:18:53 +0200

    add seller col to users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :seller
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -19,6 +19,7 @@ ActiveAdmin.register User do
       f.input :email
       f.input :password
       f.input :password_confirmation
+      f.input :seller, as: :radio
     end
     f.actions
   end
